a03e6fa8b9e961a42de3c5ede4f4eacb4e28d938
caonamshynd20122000 <caonamnd12345@gmail.com>
2025-07-08 23:30:15 +0700

    First Upload Project

---
+++ RTL/ADDER.v
@@ -0,0 +1,13 @@
+/*
+    Created             : /home/caonam/SNN_THESIS_PROJECT/RTL/ADDER.v
+    Module Name         : ADDER
+    Author              : Van Nam CAO
+    Date                : 08 - 07 - 2025
+    Description         : This module that perform 32-bit floating point addition
+*/
+
+module ADDER (
+    // FIXME: define ports
+);
+
+endmodule
